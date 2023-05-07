7c0e0aedfa5ad98cadaed89b0ecae15e1d959407
JehanKandy <jehankandy@gmail.com>
2023-05-07 05:54:24 +0530

    Update

--- src/jdbc/JDBC.java
+++ src/jdbc/JDBC.java
@@ -14,8 +14,6 @@ public class JDBC {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
-        // TODO code application logic here
-    }
+
     
 }
